0b4d6d248e84355e0f86f60ca842937d33891740
Abhishek Verma <63551058+nonuabi@users.noreply.github.com>
2021-09-09 00:31:01 +0530

    Update mongoose.js

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://abhishek:<abhishektest>@cluster0.gkjbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://abhishek:abhishektest@cluster0.gkjbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
 const db = mongoose.connection;
 
